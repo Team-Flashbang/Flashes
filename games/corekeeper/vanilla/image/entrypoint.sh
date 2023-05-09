4e3d05e243a6d6ea033948450f5e7ae4ea19b7b6
Nick Gildenstern <nick.gildenstern@web.de>
2023-05-09 16:20:22 +0000

    Core Keeper Egg

---
+++ games/corekeeper/vanilla/image/entrypoint.sh
@@ -0,0 +1,6 @@
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
